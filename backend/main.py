c44bb5d45423044c96511fad5fdf8d4fce47e97f
scopophobic <adithyanmadhu1234@gmail.com>
2025-06-18 23:31:31 +0530

    idk

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
+
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # replace with actual domain in prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 JUDGE0_URL = "https://ce.judge0.com/submissions/?base64_encoded=false&wait=true"
 
 @app.get("/")
 async def root():
     return {"message": "byteIDE is running"}
+
 
 @app.post("/run")
 async def run_code(request: Request):
@@ -16,5 +28,5 @@ async def run_code(request: Request):
         "language_id": data["language_id"],
         "stdin": data["input"]
     }
-    resp = requests.post(JUDGE0_URL, json=payload)
-    return resp.json()
+    response = requests.post(JUDGE0_URL, json=payload)
+    return response.json()
